b51cc2f0eee90ded5b80ab1e708c582f3f974b56
Noah Kupinsky <noah@kupinsky.com>
2024-06-15 15:25:24 -0400

    Outline shared house type

--- living-mile-high-types/index.d.ts
+++ living-mile-high-types/index.d.ts
@@ -1,4 +1,18 @@
-export interface House {
-    id: string
-    address: string
+export type House = {
+    onHomePage: boolean,
+    isDeveloped: boolean,
+    isForSale: boolean,
+    address: string,
+    mainPhotoUrl: string,
+    photos: string[],
+    neighborhood: string,
+    stats: HouseStats
+}
+
+export type HouseStats = {
+    houseSquareFeet?: number,
+    lotSquareFeet?: number,
+    bedrooms?: number,
+    bathrooms?: number,
+    garageSpaces?: number,
 }
