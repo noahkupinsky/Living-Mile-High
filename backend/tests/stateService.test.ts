d66ad4cc999364676616f5419f5d841c0f3f02b3
Noah Kupinsky <noah@kupinsky.com>
2024-07-10 16:57:58 -0400

    test stateService

--- backend/tests/stateService.test.ts
+++ backend/tests/stateService.test.ts
@@ -1,5 +1,7 @@
 import { services } from "~/di";
 import { StateService } from "~/@types/services";
+import HouseModel from "~/models/HouseModel";
+import GeneralDataModel from "~/models/GeneralDataModel";
 
 let stateService: StateService;
 
@@ -12,5 +14,30 @@ describe('StateService', () => {
     it('should get state', async () => {
         const state = await stateService.getState();
         expect(state).not.toBeNull();
+    })
+
+    it('should serialize state', async () => {
+        const houseData = await HouseModel.find().lean().exec();
+        const generalData = await GeneralDataModel.find().lean().exec();
+
+        const state = await stateService.serializeState();
+        const { houseData: houseDataFromState, generalData: generalDataFromState } = JSON.parse(state);
+
+        expect(houseDataFromState).toEqual(houseData);
+        expect(generalDataFromState).toEqual(generalData);
+    })
+
+    it('should deserialize state', async () => {
+        const originalHouseData = await HouseModel.find().lean().exec();
+        const originalGeneralData = await GeneralDataModel.find().lean().exec();
+
+        const state = await stateService.serializeState();
+        await stateService.deserializeState(state);
+
+        const deserializedHouseData = await HouseModel.find().lean().exec();
+        const deserializedGeneralData = await GeneralDataModel.find().lean().exec();
+
+        expect(deserializedHouseData).toEqual(originalHouseData);
+        expect(deserializedGeneralData).toEqual(originalGeneralData);
     })
 })
