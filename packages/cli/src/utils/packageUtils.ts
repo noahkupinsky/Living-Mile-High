182e817c9e4216765fb86e0b8297e22c9dbd560f
Noah Kupinsky <noah@kupinsky.com>
2024-08-11 17:40:35 -0400

    correct cli update package method

--- packages/cli/src/utils/packageUtils.ts
+++ packages/cli/src/utils/packageUtils.ts
@@ -2,7 +2,7 @@ import shell from 'shelljs';
 import { joinRoot } from '../config';
 
 export function updatePackage(packageName: string) {
-    ['frontend', 'backend', 'lmh-cli'].forEach(dir => {
+    ['frontend', 'backend', 'packages/cli'].forEach(dir => {
         shell.cd(joinRoot(dir));
         shell.exec(`yarn remove "${packageName}"`);
         shell.exec(`yarn add "${packageName}"`);
